44b017d776886c65355c627a89180e28549c083a
陈邦杰 <chenbangjie@chenbangjiedebijixingdiannao.local>
2026-07-24 23:08:16 +0800

    PWA v3.0: 7-Tab redesign (首页/计划/物料/生产/品质/客验/设定)

---
+++ sw.js
@@ -0,0 +1,47 @@
+// PMApp PWA Service Worker v5
+const CACHE_NAME = 'pmapp-pwa-v5';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('message', (e) => {
+  if (e.data === 'CLEAR_CACHE') {
+    caches.keys().then((keys) => {
+      Promise.all(keys.map((k) => caches.delete(k))).then(() => {
+        if (e.source) e.source.postMessage('CACHE_CLEARED');
+      });
+    });
+  }
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === self.location.origin) {
+    e.respondWith(
+      caches.match(e.request).then((cached) => {
+        const fetchPromise = fetch(e.request)
+          .then((resp) => {
+            if (resp.ok) {
+              const respClone = resp.clone();
+              caches.open(CACHE_NAME).then((c) => c.put(e.request, respClone));
+            }
+            return resp;
+          })
+          .catch(() => cached);
+        return cached || fetchPromise;
+      })
+    );
+  }
+});
