be7b54fcfc57a92e33407a5252036dc4e811bdc2
RyanChen7051 <3531885069@qq.com>
2026-07-26 11:19:40 +0800

    PWA v3.4.0 i18n - 7-language support - sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// PMApp PWA Service Worker v21
-const CACHE_NAME = 'pmapp-pwa-v21';
+// PMApp PWA Service Worker v22
+const CACHE_NAME = 'pmapp-pwa-v22';
 const ASSETS = ['./', './index.html', './bundle.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
