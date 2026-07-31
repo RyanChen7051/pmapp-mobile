6c0b3430e1ce6ec3bee1c683f6aa3aa2bd977d05
RyanChen7051 <3531885069@qq.com>
2026-07-31 14:54:17 +0800

    Update PWA - sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// PMApp PWA Service Worker v68
-const VERSION = 'v68';
+// PMApp PWA Service Worker v69
+const VERSION = 'v69';
 const CACHE_NAME = 'pmapp-pwa-' + VERSION;
 const ASSETS = ['./', './index.html', './bundle.js', './manifest.json', './icon-192-v2.png', './icon-512-v2.png', './apple-touch-icon-v2.png', './favicon-32-v2.png'];
 
