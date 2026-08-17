ec3795ec8761fd56524027836e076081c03404cc
PMApp <dev@pmapp.io>
2026-08-18 00:20:14 +0800

    fix: 提升 SW 版本至 v85 强制刷新缓存，配合 VAPID 公钥修复

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// PMApp PWA Service Worker v84
-const VERSION = 'v84';
+// PMApp PWA Service Worker v85
+const VERSION = 'v85';
 const CACHE_NAME = 'pmapp-pwa-' + VERSION;
 const ASSETS = ['./', './index.html', './bundle.js', './manifest.json', './icon-192-v2.png', './icon-512-v2.png', './apple-touch-icon-v2.png', './favicon-32-v2.png'];
 
