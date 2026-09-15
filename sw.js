3c55118587792deb07f379bcd0460a343aa324f1
PMApp <dev@pmapp.io>
2026-09-15 17:58:49 +0800

    style(PWA): 浮水印改为右下角小标（84px·opacity.12），bump SW v145 并预缓存水印资源

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // PMApp PWA Service Worker v101
-const VERSION = 'v144';
+const VERSION = 'v145';
 const CACHE_NAME = 'pmapp-pwa-' + VERSION;
-const ASSETS = ['./', './index.html', './bundle.js', './manifest.json', './icon-192-v2.png', './icon-512-v2.png', './apple-touch-icon-v2.png', './favicon-32-v2.png'];
+const ASSETS = ['./', './index.html', './bundle.js', './manifest.json', './pmapp-watermark.png', './icon-192-v2.png', './icon-512-v2.png', './apple-touch-icon-v2.png', './favicon-32-v2.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)).catch(() => {}));
